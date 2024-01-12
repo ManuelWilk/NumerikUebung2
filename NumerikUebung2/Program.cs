c7492f5d1b4271bd872e7e383b74ec6f97571640
s0577842 <emmanuel.wilke@student.htw-berlin.de>
2024-01-12 12:30:56 +0100

    LR-Zerlegung hinzugefügt

--- NumerikUebung2/Program.cs
+++ NumerikUebung2/Program.cs
@@ -10,38 +10,98 @@ namespace NumerikUebung2
     {
         static void Main(string[] args)
         {
-            //Matrix A erstellen:
-            Console.WriteLine("Anzahl Zeilen der 1ten Matrix engeben:");
-            int rows1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Anzahl Spalten der 1ten Matrix engeben:");
-            int cols1 = int.Parse(Console.ReadLine());
-            double[,] matrixA = CreateMatrix(rows1, cols1);
+            //Auswahl zwischen Multiplikation, LR-Zerlegung
+            Console.WriteLine("Was möchten Sie tun?"+ '\n'+ "1.Multiplikation" + '\n' + "2.LR-Zerlegung");
+            int choice = int.Parse(Console.ReadLine());
 
-            //Matrix B erstellen:
-            Console.WriteLine("Anzahl Zeilen der 2ten Matrix engeben:");
-            int rows2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Anzahl Spalten der 2ten Matrix engeben:");
-            int cols2 = int.Parse(Console.ReadLine());
-            double[,] matrixB = CreateMatrix(rows2, cols2);
+            //Multiplikation
+            if (choice == 1)
+            {
+                //Matrix A erstellen:
+                Console.WriteLine("Anzahl Zeilen der 1ten Matrix engeben:");
+                int rows1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Anzahl Spalten der 1ten Matrix engeben:");
+                int cols1 = int.Parse(Console.ReadLine());
+                double[,] matrixA = CreateMatrix(rows1, cols1);
 
-            //Matrizen zum Testen ohne Nutzereingabe
-            //double[,] matrixA = { { 1, 2, 3 }, { 6, 5, 4 }, { 7, 8, 9 } };
-            //double[,] matrixB = { { 0, 3, 4 }, { 1, 2, 3 }, { -2, 5, 6 } };
+                //Matrix B erstellen:
+                Console.WriteLine("Anzahl Zeilen der 2ten Matrix engeben:");
+                int rows2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Anzahl Spalten der 2ten Matrix engeben:");
+                int cols2 = int.Parse(Console.ReadLine());
+                double[,] matrixB = CreateMatrix(rows2, cols2);
 
-            //Ausgeben der Matrizen mit PrintMatrix
-            Console.WriteLine("Matrix A:");
-            PrintMatrix(matrixA);
-            Console.WriteLine("Matrix B:");
-            PrintMatrix(matrixB);
+                //Ausgeben der Matrizen mit PrintMatrix
+                Console.WriteLine("Matrix A:");
+                PrintMatrix(matrixA);
+                Console.WriteLine("Matrix B:");
+                PrintMatrix(matrixB);
 
-            //Multiplizieren der Matrizen und Ausgabe mit MultMatrix
-            Console.WriteLine("Produktmatrix:");
-            PrintMatrix(MultMatrix(matrixA, matrixB));
+                //Multiplizieren der Matrizen und Ausgabe mit MultMatrix
+                Console.WriteLine("Produktmatrix:");
+                PrintMatrix(MultMatrix(matrixA, matrixB));
 
-            //Programm beenden
-            Console.WriteLine("Zum beenden beliebige Taste drücken...");
-            Console.ReadLine();
+                //Programm beenden
+                Console.WriteLine("Zum beenden beliebige Taste drücken...");
+                Console.ReadLine();
+            }
+
+            //LR-Zerlegung
+            if (choice == 2)
+            {
+                Console.WriteLine("Dimension der Matrix eingeben");
+                int dim = int.Parse(Console.ReadLine());
+                double[,] matrix = CreateMatrix(dim,dim);
+                double[,] Lm = new double[dim, dim];
+                double[,] Rm = new double[dim, dim];
+
+                for (int i = 0; i < dim; i++)
+                {
+                    //R-Matrix
+                    for (int j = i; j < dim; j++)
+                    {
+                        double sum = 0;
+                        for (int k = 0; k < i; k++)
+                        {
+                            sum += Lm[i, k] * Rm[k, j];
+                        }
+                        Rm[i, j] = matrix[i, j] - sum;
+                    }
+
+                    //L-Matrix
+                    for (int k = i; k < dim; k++)
+                    {
+                        if (i == k)
+                        {
+                            Lm[i, i] = 1;
+                        }
+                        else
+                        {
+                            double sum = 0;
+                            for (int j = 0; j < i; j++)
+                            {
+                                sum += Lm[k, j] * Rm[j, i];
+                            }
+                            Lm[k, i] = (matrix[k, i] - sum) / Rm[i, i];
+                        }
+                    }
+                }
+                Console.WriteLine("Ihre Matrix:");
+                PrintMatrix(matrix);
+                Console.WriteLine("L-Matrix:");
+                PrintMatrix(Lm);
+                Console.WriteLine("R-Matrix");
+                PrintMatrix(Rm);
+                Console.WriteLine("Zum beenden beliebige Taste drücken...");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Eingabe");
+                Console.ReadLine();
+            }
         }
+
 
         //Matrizen Ausgeben
         static void PrintMatrix(double[,] matrix)
@@ -83,6 +143,7 @@ namespace NumerikUebung2
             }
         }
 
+        //Matrizen erstellen
         static double[,] CreateMatrix(int rows, int cols)
         {
             double[,] matrix = new double[rows, cols];
